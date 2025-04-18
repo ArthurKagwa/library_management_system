46af59982c28703a16be3bd77ba12afaaba3b649
Arthur Asasira <asasiraarthur@gmail.com>
2025-04-18 13:12:29 +0300

    refactor: remove unused links from side navigation

--- resources/views/components/side-navigation.blade.php
+++ resources/views/components/side-navigation.blade.php
@@ -32,20 +32,6 @@
             </div>
 
             <div class="mt-8">
-                <!-- Common links -->
-                <a href="{{ route('dashboard') }}"
-                   class="flex items-center px-4 py-2 mb-2 rounded-md {{ request()->routeIs('dashboard') ? 'bg-secondary-accent text-white' : 'text-primary bg-secondary dark:text-primary-dark  dark:bg-secondary-dark hover:bg-gray-100 dark:hover:bg-primary' }}">
-                    <x-icon name="check-circle" class="w-5 h-5 mr-3" />
-                    <span>Dashboard</span>
-                </a>
-
-                <a href="{{ route('profile.edit') }}"
-                   class="flex items-center px-4 py-2 mb-2 rounded-md {{ request()->routeIs('profile.edit') ? 'bg-secondary-accent text-primary-dark' : 'text-primary bg-secondary dark:text-primary-dark  dark:bg-secondary-dark hover:bg-gray-100 dark:hover:bg-primary' }}">
-                    <svg class="w-5 h-5 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
-                    </svg>
-                    <span>Profile</span>
-                </a>
 
                 <!-- Member links -->
                 @if(auth()->check() && auth()->user()->hasRole('member'))
